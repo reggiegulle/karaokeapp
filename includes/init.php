afe1e6bb21040fc02bfec990b8a35fa733fe8f82
Reggie Gulle <reggie.gulle@gmail.com>
2014-12-23 19:00:57 +1100

    Added initialisation file.
    This is a new initialisation file for querying the database.

---
+++ includes/init.php
@@ -0,0 +1,28 @@
+<?php
+//this file will be required on every PHP page
+session_start();
+
+$GLOBALS['config'] = [
+	'mysql' => [
+		'host' => '',
+		'username' => '',
+		'password' => '',
+		'db' => ''
+	],
+	'remember' => [
+		'cookie_name' => '',
+		'cookie_expiry' => ''
+	],
+	'session' => [
+		'session_name' => '',
+		'token_name' => ''
+	]
+];
+
+spl_autoload_register(function($class){
+	require_once '../classes/' . $class . '.php';
+});
+
+require_once 'sanitize.php';
+
+?>
