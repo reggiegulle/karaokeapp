b0a84afb1f63a4083a1dcc9fef34e24c8b2c2e29
Reggie Gulle <reggie.gulle@gmail.com>
2014-12-23 18:43:47 +1100

    Added style changes to the datatable jquery plugin
    This commit includes basic changes to the styling of the videos HTML
    table.

--- js/karaoke.js
+++ js/karaoke.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
 
- $("#videos_datatable").dataTable({
+	$("#videos_datatable").dataTable({
         "processing": true,
         "serverSide": true,
-        "ajax": "ajax/table_processing.php"
+        "ajax": "ajax/table_processing.php",
+		"dom": "<\"col-xs-8\"l><\"col-xs-4\"f><\"col-sm-8 col-xs-12\"i><\"col-sm-4 col-xs-12\"p><\"col-xs-12\"t><\"col-sm-8 col-xs-12\"i><\"col-sm-4 col-xs-12\"p>r",
+		"columnDefs":[
+				{"orderable": false, "targets":[4, 10]}
+			],
+		"drawCallback": function (settings) {
+				populatelists();
+			}
     });
+	
+	function populatelists() {
+		$("#videos_datatable tbody tr").each( function () {
+		
+			//declare videos_datatable var
+			//to correspond to
+			//datatables API instance
+			var videos_datatable = $("#videos_datatable").DataTable();
+			
+			var videoidnode = videos_datatable.cell(this, 4).node();
+			var videoid = $(videoidnode).html();
 
+			var songtitlenode = videos_datatable.cell(this, 1).node();
+			var songtitle = $(songtitlenode).html();
+	
+			$(videoidnode).html('<img src="https://i3.ytimg.com/vi/' + videoid + '/default.jpg" alt="' + songtitle + ' thumbnail" width="120px" height="90px" longdesc="Thumbnail for the Youtube karaoke video of ' + songtitle + '" />');
+
+		});
+	}
 
 });
