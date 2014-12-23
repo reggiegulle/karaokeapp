a1e71a246f754ff9281f341beef37a85638ca8f9
Reggie Gulle <reggie.gulle@gmail.com>
2014-12-23 18:55:19 +1100

    Added new php pages and files
    In this commit, I include the "sanitize.php" file and the
    "add_video.php" page.

---
+++ public/add_video.php
@@ -0,0 +1,27 @@
+<h1>Add New Video</h1>
+<form id="addnewvideo" action="" method="POST">
+	<label for="song_title"><p>Song Title:</p></label>
+	<input type="text" id="song_title" name="song_title" form="addnewvideo" value="" />
+	<label for="composer"><p>Song Composer:</p></label>
+	<input type="text" id="composer" name="composer" form="addnewvideo" value="" />
+	<label for="performed_by"><p>Song Performer:</p></label>
+	<input type="text" id="performed_by" name="performed_by" form="addnewvideo" value="" />
+	<label for="videoid"><p>YouTube Video ID:</p></label>
+	<input type="text" id="videoid" name="videoid" form="addnewvideo" value="" />
+	<label for="source_album"><p>Source Album:</p></label>
+	<input type="text" id="source_album" name="source_album" form="addnewvideo" value="" />
+	<label for="year_of_release"><p>Year of Release:</p></label>
+	<input type="text" id="year_of_release" name="year_of_release" form="addnewvideo" value="" />
+	<label for="genre"><p>Genre:</p></label>
+	<input type="text" id="genre" name="genre" form="addnewvideo" value="" />
+	<label for="country_of_origin"><p>Country of Origin:</p></label>
+	<input type="text" id="country_of_origin" name="country_of_origin" form="addnewvideo" value="" />
+	<label for="running_time"><p>Running Time:</p></label>
+	<input type="text" id="running_time_min" name="running_time_min" form="addnewvideo" value="" /> min :
+	<input type="text" id="running_time_sec" name="running_time_sec" form="addnewvideo" value="" /> sec
+	<label for="lyrics"><p>Song Lyrics:</p></label>
+	<textarea id="lyrics" name="lyrics"></textarea>
+	<br />
+	<input type="submit" value="Create Entry" />
+	<a href="#">Cancel</a>
+</form>
