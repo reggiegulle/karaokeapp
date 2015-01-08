3e3cc274588ad2539e5d7d335b2be988171dfb44
Reggie Gulle <reggie.gulle@gmail.com>
2015-01-08 22:00:50 +1100

    Altered timing of masthead animation.

--- js/karaoke.style.js
+++ js/karaoke.style.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	var mastheadAnim = new TimelineLite();
 	
 	mastheadAnim
-		.fromTo( "#masthead", 0.25, {opacity: "0", color:"rgb(89, 66, 49)", textShadow: "rgb(255, 255, 255) -1px -1px 1px, rgb(255, 255, 0) -1px -1px 1px, rgb(255, 128, 0) 1px 1px 1px, rgb(255, 0, 0) 1px 2px 1px"}, {opacity: 0.3}, "+=1" )
+		.fromTo( "#masthead", 0.25, {opacity: "0", color:"rgb(89, 66, 49)", textShadow: "rgb(255, 255, 255) -1px -1px 1px, rgb(255, 255, 0) -1px -1px 1px, rgb(255, 128, 0) 1px 1px 1px, rgb(255, 0, 0) 1px 2px 1px"}, {opacity: 0.3} )
 		.to( "#masthead", 0.25, {opacity: "0"} )
 		.to( "#masthead", 0.10, {opacity: "0.65"} )
 		.to( "#masthead", 0.10, {opacity: "0.3", textShadow: "rgb(255, 255, 255) -1px -1px 1px, rgb(255, 255, 0) -1px -2px 2px, rgb(255, 128, 0) 1px 1px 3px, rgb(255, 0, 0) 1px 2px 3px"}, "+=0.1" )
@@ -27,7 +27,7 @@ $(document).ready(function(){
 		.to( "#masthead", 0.10, {opacity: "0.65"} )
 		.to( "#masthead", 0.10, {opacity: "0"}, "+=0.1" )
 		.to( "#masthead", 0.10, {opacity: "1"} )
-		.set( "#masthead", {opacity: 1}, "+=30" )
+		.set( "#masthead", {opacity: 1}, "+=10" )
 		.call(animRestart);
 	
 	function animRestart(){
