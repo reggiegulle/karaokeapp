7df7e4d10cec1897f382d38163172f9c83893fda
Reggie Gulle <reggie.gulle@gmail.com>
2015-01-08 21:03:42 +1100

    Modified masthead text color and text shadow

--- js/karaoke.style.js
+++ js/karaoke.style.js
@@ -12,22 +12,22 @@ $(document).ready(function(){
 	var mastheadAnim = new TimelineLite();
 	
 	mastheadAnim
-		.fromTo( "#masthead", 0.25, {opacity: 0}, {opacity: 0.3}, "+=2" )
-		.to( "#masthead", 0.25, {opacity: 0} )
-		.to( "#masthead", 0.10, {opacity: 0.65} )
-		.to( "#masthead", 0.10, {opacity: 0.3}, "+=0.1" )
-		.to( "#masthead", 0.10, {opacity: 0.65} )
-		.to( "#masthead", 0.10, {opacity: 0.3} )
-		.to( "#masthead", 0.10, {opacity: 0.65} )
-		.to( "#masthead", 0.10, {opacity: 0.3} )
-		.to( "#masthead", 0.10, {opacity: 0.65} )
-		.to( "#masthead", 0.20, {opacity: 0.3} )
-		.to( "#masthead", 0.10, {opacity: 1} )
-		.to( "#masthead", 0.10, {opacity: 0}, "+=0.1" )
-		.to( "#masthead", 0.10, {opacity: 0.65} )
-		.to( "#masthead", 0.10, {opacity: 0}, "+=0.1" )
-		.to( "#masthead", 0.10, {opacity: 1} )
-		.set( "#masthead", {opacity: 1}, "+=10" )
+		.fromTo( "#masthead", 0.25, {opacity: "0", color:"rgb(89, 66, 49)", textShadow: "rgb(255, 255, 255) -1px -1px 1px, rgb(255, 255, 0) -1px -1px 1px, rgb(255, 128, 0) 1px 1px 1px, rgb(255, 0, 0) 1px 2px 1px"}, {opacity: 0.3}, "+=1" )
+		.to( "#masthead", 0.25, {opacity: "0"} )
+		.to( "#masthead", 0.10, {opacity: "0.65"} )
+		.to( "#masthead", 0.10, {opacity: "0.3", textShadow: "rgb(255, 255, 255) -1px -1px 1px, rgb(255, 255, 0) -1px -2px 2px, rgb(255, 128, 0) 1px 1px 3px, rgb(255, 0, 0) 1px 2px 3px"}, "+=0.1" )
+		.to( "#masthead", 0.10, {opacity: "0.65"} )
+		.to( "#masthead", 0.10, {opacity: "0.3"} )
+		.to( "#masthead", 0.10, {opacity: "0.65"} )
+		.to( "#masthead", 0.10, {opacity: "0.3"} )
+		.to( "#masthead", 0.10, {opacity: "0.65"} )
+		.to( "#masthead", 0.20, {opacity: "0.3"} )
+		.to( "#masthead", 0.10, {opacity: "1"} )
+		.to( "#masthead", 0.10, {opacity: "0", color:"rgb(245, 152, 81)", textShadow: "rgb(255, 255, 255) 0px -1px 4px, rgb(255, 255, 0) 0px -2px 10px, rgb(255, 128, 0) 0px -10px 20px, rgb(255, 0, 0) 0px -18px 40px"}, "+=0.1" )
+		.to( "#masthead", 0.10, {opacity: "0.65"} )
+		.to( "#masthead", 0.10, {opacity: "0"}, "+=0.1" )
+		.to( "#masthead", 0.10, {opacity: "1"} )
+		.set( "#masthead", {opacity: 1}, "+=30" )
 		.call(animRestart);
 	
 	function animRestart(){
